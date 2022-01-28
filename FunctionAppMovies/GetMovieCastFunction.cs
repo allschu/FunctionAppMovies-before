b970a9c45120a2ff6c8072578994c866b4d7d35c
Allard Schuurmans <a.schuurmans@4DotNet.nl>
2022-01-28 12:02:52 +0100

    Add project files.

---
+++ FunctionAppMovies/GetMovieCastFunction.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace FunctionAppMovies
+{
+    public class GetMovieCastFunction
+    {
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+
+        public GetMovieCastFunction(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri(_configuration["MovieDbUrl"]);
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer { _configuration["MovieDbBearer"] }");
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+           
+        }
+
+        [FunctionName("GetMovieCastFunction")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log, CancellationToken cancellationToken)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string id = req.Query["id"];
+
+            var response = await _httpClient.GetAsync($"3/movie/{id}/credits", cancellationToken).ConfigureAwait(false);
+
+            var content = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+
+            return new OkObjectResult(content);
+        }
+    }
+}
---
+++ FunctionAppMovies/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+[assembly: FunctionsStartup(typeof(FunctionAppMovies.Startup))]
+namespace FunctionAppMovies
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+        }
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            FunctionsHostBuilderContext context = builder.GetContext();
+            builder.ConfigurationBuilder
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, "local.settings.json"), optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
