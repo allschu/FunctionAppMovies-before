7a9788d1db057f6d90597d4f47738dfeba452848
Allard Schuurmans <allard.schuurmans@gmail.com>
2022-02-19 12:42:44 +0100

    added person

--- FunctionAppMovies/GetPersonFunction.cs
+++ FunctionAppMovies/GetPersonFunction.cs
@@ -1,25 +1,26 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
-using System.Threading;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using FunctionAppMovies.Models;
-using Newtonsoft.Json;
 
 namespace FunctionAppMovies
 {
-    public class GetMovieCastFunction
+    public class GetPersonFunction
     {
         private readonly IConfiguration _configuration;
         private readonly HttpClient _httpClient;
 
-        public GetMovieCastFunction(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        public GetPersonFunction(IConfiguration configuration, IHttpClientFactory httpClientFactory)
         {
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
 
@@ -29,19 +30,18 @@ namespace FunctionAppMovies
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        [FunctionName("GetMovieCastFunction")]
+        [FunctionName("GetPersonFunction")]
         public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log, CancellationToken cancellationToken)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string id = req.Query["id"];
 
-            var response = await _httpClient.GetAsync($"3/movie/{id}/credits", cancellationToken).ConfigureAwait(false);
+            var response = await _httpClient.GetAsync($"3/person/{id}", cancellationToken).ConfigureAwait(false);
 
             var content = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
 
-            return new OkObjectResult(JsonConvert.DeserializeObject<CastSelection>(content));
+            return new OkObjectResult(JsonConvert.DeserializeObject<Person>(content));
         }
-       
     }
 }
---
+++ FunctionAppMovies/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionAppMovies.Models
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string birthday { get; set; }
+        public string known_for_department { get; set; }
+        public string deathday { get; set; }
+        public string[] also_known_as { get; set; }
+        public string biography { get; set; }
+        public string place_of_birth { get; set; }
+        public string profile_path  { get; set; }
+        public string imdb_id { get; set; }
+        public string homepage { get; set;}
+    }
+}
